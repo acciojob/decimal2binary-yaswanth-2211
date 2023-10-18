d48281150d5a5257020dfeb3f3b420b0dbdf7b40
yaswanth-2211 <145182914+yaswanth-2211@users.noreply.github.com>
2023-10-18 10:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let sum=0;
+  for(i=0;i++;i<num.length){
+	  sum=sum+(Math.pow(2,i))*num.charAt(num.length-i);
+  }
 }
 
 window.decimalToBinary = decimalToBinary;
