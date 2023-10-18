d845e4ab3d8540910dacd86a73cba92cd2f6e108
yaswanth-2211 <145182914+yaswanth-2211@users.noreply.github.com>
2023-10-18 11:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function decimalToBinary(num) {
-  //Write you code here
-	return parseInt(num,10);
+	let a="";
+  while(num>=0){
+      if(num==0){
+	      a=a+0;
+	      return a.split("").reverse().join("");
+	  }
+	  else if(num==1){
+	      a=a+1;
+	      return a.split("").reverse().join("");
+	  }
+	  else if(num%2==0){
+		  a=a+0
+		  num=num/2;
+		  num=parseInt(num/2);
+	  }
+	  else{
+		  a=a+1
+		  num=parseInt(num/2);
+		  console.log(num);
+	  }
+  }
 }
 
 window.decimalToBinary = decimalToBinary;
